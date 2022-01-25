9fc65deba10d1a293aa8be5e751094c45d10670e
enesonmez <enesonmezx@gmail.com>
2022-01-25 13:00:00 +0300

    heroku config

--- app.js
+++ app.js
@@ -76,5 +76,8 @@ app.get('/weekly', function(req, res) {
   });
 });
 
-app.listen(8080);
-console.log('Server is listening on port 8080');
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}`);
+  console.log('Press Ctrl+C to quit.');
+});
